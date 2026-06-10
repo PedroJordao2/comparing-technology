931303d418ebcde41925f60e96e2f5e74cebbb10
Pedro Silva <pedrojordao22@hotmail.com>
2026-06-09 22:23:03 -0300

    Delete music_streaming_project/projeto directory

--- music_streaming_project/projeto/grpc_service/generate_stubs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Execute este script dentro da pasta grpc_service/ para gerar os stubs Python
-echo "Gerando stubs gRPC a partir de music.proto..."
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. music.proto
-echo "Stubs gerados: music_pb2.py e music_pb2_grpc.py"
